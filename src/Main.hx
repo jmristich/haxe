04c7677f60270447d2889274dec9c6b6e82741db
Jason Ristich <jmristich@gmail.com>
2018-01-11 13:57:26 -0700

    Updating code to include library

--- src/Main.hx
+++ src/Main.hx
@@ -1,34 +1,17 @@
-import h2d.Text;
-import hxd.Res;
-import hxd.res.Font;
-import js.Lib;
-
 class Main extends hxd.App {
-        var text : Text;
-
-		// Called on creation
+        var bmp : h2d.Bitmap;
+        
         override function init() {
-			// Initialize all loaders for embeded resources
-			Res.initEmbed();
-			// Create an instance of wireframe.tff located in our res folder, then create a font2d of 128pt size
-			var font = Res.firacode.build(128);
-			// Create a new text object using the newly created font, parented to the 2d scene
-            text = new Text(font, s2d);
-			// Assign the text
-			text.text = "Hello World";
-			// Make it read, using hex code ( RR GG BB, each two hex characters represent an RGB value from 0 - 255 )
-			text.textColor = 0xFF0000;
+            var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
+            bmp = new h2d.Bitmap(tile, s2d);
+            bmp.x = s2d.width * 0.5;
+            bmp.y = s2d.height * 0.5;
         }
-
-		// Called each frame
+        
         override function update(dt:Float) {
-			// simply scale our text object up until it's 3x normal size, repeat forever
-			var scaleAmount = 0.01;
-			if (text.scaleX < 3.0) text.setScale(text.scaleX + scaleAmount);
-			else text.setScale(1);
-			
+            bmp.rotation += 0.1;
         }
-
+        
         static function main() {
             new Main();
         }
